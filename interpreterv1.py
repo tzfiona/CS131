31bbc053b64f048626256e6582d0eca0e6037b8a
Fiona TZ <107426453+fionatz@users.noreply.github.com>
2025-10-14 02:27:01 -0700

    worked on init & run function

---
+++ interpreterv1.py
@@ -0,0 +1,40 @@
+from intbase import InterpreterBase
+from brewparse import parse_program
+
+class Interpreter(InterpreterBase):
+  def __init__(self, console_output=True, inp=None, trace_output=False):
+    super().__init__(console_output, inp)
+  
+  def run(self, program):
+    ast = parse_program(program, False)
+    self.variable_name_to_value = {}
+    
+    #main_func_node = get_main_func_node(ast)
+    if elem.elem_type == "program":
+    #find dict:func, find dict.name =="main", 
+
+  def run_func(func_node):
+
+
+  def run_statement(statement_node):
+
+
+  def do_assignment(statement_node):
+
+
+  def evaluate_expression(expression_node):
+
+
+def main():
+  program = """def main() {
+                  var x;
+                  x = 11 + 32;
+                  print("The result is: ", x);
+            }"""
+  
+  interpreter = Interpreter()
+  interpreter.run(program)
+
+if __name__ == "__main__":
+  main()
+  
