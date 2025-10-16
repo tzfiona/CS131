e4d6de495baa1b682f1208fa99a94aa7e1b58e2c
Fiona TZ <107426453+fionatz@users.noreply.github.com>
2025-10-16 01:57:50 -0700

    all function completed

--- interpreterv1.py
+++ interpreterv1.py
@@ -1,10 +1,12 @@
 from intbase import InterpreterBase
 from brewparse import parse_program
 
+
 class Interpreter(InterpreterBase):
   def __init__(self, console_output=True, inp=None, trace_output=False):
     super().__init__(console_output, inp)
   
+
   def run(self, program):
     ast = parse_program(program, False)
     self.variable_name_to_value = {}
@@ -22,11 +24,13 @@ class Interpreter(InterpreterBase):
               "No main() function was found",
             )
 
+
   def run_func(self, func_node):
     print("run func starts now~~~~~~~~~~~~~~~~~~~~~~~") ###
     if func_node.elem_type == "func": 
       for stmt_node in func_node.get("statements"):
         self.run_statement(stmt_node)
+
 
   def run_statement(self, statement_node):
     print("run statement starts now~~~~~~~~~~~~~~~~~~~~~~~") ###
@@ -34,53 +38,33 @@ class Interpreter(InterpreterBase):
     if statement_node.elem_type == "vardef": 
       for i in statement_node.get("name"):
         self.variable_name_to_value[i] = None
-        print({i}) ###
+        print("saves var", i, "to dict") ###
 
-    elif statement_node.elem_type == "=": #FIXXXXXXX
-      print("is assignment") ###
+    elif statement_node.elem_type == "=": 
       a = statement_node.get("var")
       exp_node = statement_node.get("expression")
-      b = self.evaluate_expression(exp_node)
+      b = self.do_assignment(statement_node)
       self.variable_name_to_value[a] = b
-      print({a}, {b}) ###
+      print("saves", b, "to", a) ###
 
     elif statement_node.elem_type == "fcall": #FIXXXXXXX
-      print("is function call") ###
-      m = statement_node.get("name")
-      n = statement_node.get("args")
-      self.variable_name_to_value[m] = n
-      print({m}, n) ###
-      for k in n:
-        '''
-        if k.elem_type == "string": 
-          print("!!!string") ###
-          self.do_assignment(k)
-        elif k.elem_type == "qname": 
-          print("!!!qname") ###
-        '''
-        self.evaluate_expression(k)
-        
+      if statement_node.get("name") == "print":
+        print_statement = []
+        for k in statement_node.get("args"):
+          statement_item = self.evaluate_expression(k)
+          print_statement.append(statement_item)
+        print(*print_statement)     
+
 
   def do_assignment(self, statement_node):
-    '''
-    target_var_name = get_target_variable_name(statement_node)
-    if !var_name_exists(target_var_name):
-      generate_error("Undefined variable " + target_var_name)
-    source_node = get_expression_node(statement_node)
-    resulting_value = evaluate_expression(source_node)
-    this.variable_name_to_value[target_var_name] = resulting_value
-    '''
     print("do assigment starting now~~~~~~~~~~~~~~~~~~~~~~~") ###
-
-# target_var_name + statement_node.get("name")
-# if ___.dict(target_var_name)!= "target_var_name":
-    # print("Undefined variable:", target_var_name)
-# source_node = statement_node.get("expression")
-# resulting_value = evaluate_expression(source_node)
-# self.variable_name_to_value[target_var_name] = resulting_value
-
-    
-
+    target_var_name = statement_node.get("var")
+    if target_var_name not in self.variable_name_to_value:
+      print("Undefined variable:", target_var_name)
+    source_node = statement_node.get("expression")
+    resulting_value = self.evaluate_expression(source_node)
+    self.variable_name_to_value[target_var_name] = resulting_value
+    return resulting_value
 
 
   def evaluate_expression(self, expression_node):
@@ -95,7 +79,7 @@ class Interpreter(InterpreterBase):
     elif (expression_node.elem_type == "+") or (expression_node.elem_type == "-"): 
       op1 = self.evaluate_expression(expression_node.get("op1")) #must use self.evaluate_expression to recursivly find the value instead of just the child node
       op2 = self.evaluate_expression(expression_node.get("op2"))
-      print(op1, "yayaya", op2) ###
+      print("op1 and op2:", op1, op2) ###
       if expression_node.elem_type == "+":
         print("op1 + op2 =", op1 + op2) ###
         return op1 + op2
@@ -108,7 +92,7 @@ def main():
   program = """def main() {
                   var x;
                   x = 11 + 32;
-                  print("The result is: ", x);
+                  print("The result is:", x);
             }"""
   
   interpreter = Interpreter()
